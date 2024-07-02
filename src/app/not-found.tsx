5d72d208befbd2002b2a20e69ae78e18ef65e8f1
dharti4 <dharti4@gmail.com>
2024-07-02 14:24:57 +0530

    not found page added to handle unwanted routes

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex gap-y-3 justify-center flex-col items-center h-96 px-5">
+     <p className="text-6xl font-poppins-bold">404</p>
+     <p className="uppercase">OPPS! Page not found</p>
+     <p>Sorry, the page you are looking for doesn't exists.</p>
+      <Link href="/" className="bg-primary px-5 py-2 rounded font-semibold my-5">Return Home</Link>
+    </div>
+  );
+}
